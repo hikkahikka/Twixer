2ae2ff5066deade70be5a698c9b090eecd1506aa
hikkahikka <93731664+hikkahikka@users.noreply.github.com>
2022-11-05 22:19:38 +0300

    Navigation Button

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using Twixer.Core;
 
 namespace Twixer.MVVM.ViewModel
@@ -11,10 +12,16 @@ namespace Twixer.MVVM.ViewModel
     {
 
         public RelayCommand PrivacyViewCommand { get; set; }
+
         public RelayCommand InfoAboutSystemCommand { get; set; }
+
+        public RelayCommand OnCloseButtonClickCommand { get; set; }
+
+        public RelayCommand OnTurnButtonClickCommand { get; set; }
 
 
         public PrivacyViewModel PrivacyVM { get; set; }
+
         public InfoAboutSystemViewModel InfoAboutSystemVM { get; set; }
 
         private object _currentView;
@@ -34,7 +41,7 @@ namespace Twixer.MVVM.ViewModel
             PrivacyVM = new PrivacyViewModel();
             InfoAboutSystemVM = new InfoAboutSystemViewModel();
             CurrentView = PrivacyVM;
-
+            
             PrivacyViewCommand = new RelayCommand(o =>
             {
                 CurrentView = PrivacyVM;
@@ -44,6 +51,14 @@ namespace Twixer.MVVM.ViewModel
             {
                 CurrentView = InfoAboutSystemVM;
             });
+
+
+            OnCloseButtonClickCommand = new RelayCommand(o => Application.Current.Shutdown());
+            OnTurnButtonClickCommand = new RelayCommand(o => {
+                Application.Current.MainWindow.WindowState = WindowState.Minimized;
+                });
+
         }
+        
     }
 }
