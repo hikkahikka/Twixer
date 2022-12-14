d5589b3266bd024fba358bd388a29c60d9e5ebb7
hikkahikka <93731664+hikkahikka@users.noreply.github.com>
2022-12-15 02:04:32 +0300

    very sad summary
    я хотел быстренько сделать InfoAboutSystem а получилось что я часа 3 делал только получение данных об ос, и то, в черновике(

--- MVVM/ViewModel/InfoAboutSystemViewModel.cs
+++ MVVM/ViewModel/InfoAboutSystemViewModel.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Twixer.MVVM.ViewModel
 {
-    class InfoAboutSystemViewModel
+    class InfoAboutSystemViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        private void OnProperyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+         
+
+        public string OS;
     }
 }
