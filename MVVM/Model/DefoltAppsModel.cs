db0369c958793741a6dccf2678dd51d377eebb53
hikkahikka <93731664+hikkahikka@users.noreply.github.com>
2023-03-15 03:58:57 +0300

    defapp

---
+++ MVVM/Model/DefoltAppsModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Twixer.MVVM.Model
+{
+    internal class DefoltAppsModel
+    {
+       
+    }
+}
