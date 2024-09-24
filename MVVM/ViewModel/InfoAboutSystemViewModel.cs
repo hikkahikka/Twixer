d634bd21ab62071414876cd8085adbcc11eb89d7
Leonid Samoshenko <samoshenkoleonid@yandex.ru>
2024-09-24 17:15:11 +0300

    save to file

--- MVVM/ViewModel/InfoAboutSystemViewModel.cs
+++ MVVM/ViewModel/InfoAboutSystemViewModel.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 using Prism;
 
 using Twixer.MVVM.Model;
+using System.IO;
+using Newtonsoft.Json;
+using System.Windows.Input;
 
 namespace Twixer.MVVM.ViewModel
 {
@@ -20,7 +23,50 @@ namespace Twixer.MVVM.ViewModel
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
         }
 
+        class SysInfo
+        {
+            public string OperationSystem
+            {
+                get;
+                set;
+            }
 
+            public string Motherboard
+            {
+                get;
+                set;
+            }
+
+            public string CPU
+            {
+                get;
+                set;
+            }
+
+            public string RAM
+            {
+                get;
+                set;
+            }
+
+            public string GPU
+            {
+                get;
+                set;
+            }
+
+            public string TimeOfWork
+            {
+                get;
+                set;
+            }
+
+            public string IPAddress
+            {
+                get;
+                set;
+            }
+        };
 
         private string _buffOperationSystem;
         private string _buffMotherboard;
@@ -29,7 +75,6 @@ namespace Twixer.MVVM.ViewModel
         private string _buffGPU;
         private string _buffTimeOfWork;
         private string _buffIPAddress;
-
 
         public string OperationSystem
         {
@@ -101,75 +146,84 @@ namespace Twixer.MVVM.ViewModel
             }
         }
 
+        public DelegateCommand OperationSystemCommand { get; set; }
+        public DelegateCommand MotherboardCommand { get; set; }
+        public DelegateCommand CPUCommand { get; set; }
+        public DelegateCommand RAMCommand { get; set; }
+        public DelegateCommand GPUCommand { get; set; }
+        public DelegateCommand TimeOfWorkCommand { get; set; }
+        public DelegateCommand IPAddressCommand { get; set; }
 
 
-        public DelegateCommand GetOperationSystemCommand { get; set; }
-        public DelegateCommand GetMotherboardCommand { get; set; }
-        public DelegateCommand GetCPUCommand { get; set; }
-        public DelegateCommand GetRAMCommand { get; set; }
-        public DelegateCommand GetGPUCommand { get; set; }
-        public DelegateCommand GetTimeOfWorkCommand { get; set; }
-        public DelegateCommand GetIPAddressCommand { get; set; }
-
-
-        private void GetOperationSystem()
+        private void SetOperationSystem()
         {
-            
             OperationSystem =  new InfoAboutSystemModel().GetOperationSystemInfo();  
-
         }
-        private void GetMotherboard()
+        private void SetMotherboard()
         {
-
             Motherboard = new InfoAboutSystemModel().GetMotherboardInfo();
-
         }
-        private void GetCPU()
+        private void SetCPU()
         {
             CPU = new InfoAboutSystemModel().GetCPUInfo();
         }
-        private void GetRAM()
+        private void SetRAM()
         {
             RAM = new InfoAboutSystemModel().GetRAMInfo();
         }
-        private void GetGPU()
+        private void SetGPU()
         {
             GPU = new InfoAboutSystemModel().GetGPUInfo();
         }
-        private void GetTimeOfWork()
+        private void SetTimeOfWork()
         {
             TimeOfWork = new InfoAboutSystemModel().GetTimeOfWorkInfo();
         }
-        private void GetIPAddress()
+        private void SetIPAddress()
         {
             IPAddress = new InfoAboutSystemModel().GetIPAddressInfo();
         }
 
         public InfoAboutSystemViewModel()
         {
-            // TODO: make it async
+            const string json_path = "info_about_system.json";
 
-            GetOperationSystemCommand = new DelegateCommand(() => GetOperationSystem());
-            GetMotherboardCommand = new DelegateCommand(() => GetMotherboard());    
-            GetCPUCommand= new DelegateCommand(() => GetCPU());
-            GetRAMCommand= new DelegateCommand(() => GetRAM());
-            GetGPUCommand= new DelegateCommand(() => GetGPU());
-            GetTimeOfWorkCommand=new DelegateCommand(() => GetTimeOfWork());
-            GetIPAddressCommand = new DelegateCommand(() => GetIPAddress());
-
-            new Task( ()=>
+            OperationSystemCommand = new DelegateCommand(() => SetOperationSystem());
+            MotherboardCommand = new DelegateCommand(() => SetMotherboard());    
+            CPUCommand= new DelegateCommand(() => SetCPU());
+            RAMCommand= new DelegateCommand(() => SetRAM());
+            GPUCommand= new DelegateCommand(() => SetGPU());
+            TimeOfWorkCommand=new DelegateCommand(() => SetTimeOfWork());
+            IPAddressCommand = new DelegateCommand(() => SetIPAddress());
+            
+            if (File.Exists(json_path))
             {
-                GetOperationSystem();
-                GetMotherboard();
-                GetCPU();
-                GetRAM();
-                GetGPU();
-                GetTimeOfWork();
-                GetIPAddress();
-            }).Start();
+                SysInfo sysinfo = JsonConvert.DeserializeObject<SysInfo>(File.ReadAllText(json_path));
+                OperationSystem = sysinfo.OperationSystem;
+                CPU = sysinfo.CPU;
+                RAM = sysinfo.RAM;
+                GPU = sysinfo.GPU;
+                Motherboard= sysinfo.Motherboard;
+                IPAddress = sysinfo.IPAddress;
+                TimeOfWork = sysinfo.TimeOfWork;
+            }
+            else
+            {
+                new Task(() =>
+                {
+                    SetOperationSystem();
+                    SetMotherboard();
+                    SetCPU();
+                    SetRAM();
+                    SetGPU();
+                    SetTimeOfWork();
+                    SetIPAddress();
 
-        } 
-
-
+                    string json = JsonConvert.SerializeObject(this);
+                    try { File.WriteAllText(json_path, json); }
+                    catch { }
+                }).Start();
+            }
+        }
     }
 }
